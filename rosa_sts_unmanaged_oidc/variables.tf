3155f1e4556c86c5c8fe8affa901dd29a578428f
Andy Repton <andy.repton@redhat.com>
2023-11-06 18:13:01 +0100

    Removing redhat aws account ID as it's not used

--- rosa_sts_unmanaged_oidc/variables.tf
+++ rosa_sts_unmanaged_oidc/variables.tf
@@ -1,9 +1,3 @@
-#Common variables
-variable "redhat_aws_account_id" {
-  type    = string
-  default = "710019948333"
-}
-
 variable "rosa_openshift_version" {
   type        = string
   default     = "4.13"
