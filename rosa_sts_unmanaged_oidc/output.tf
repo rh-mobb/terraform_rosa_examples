2c23feadea44c5119f7f6dbb5b7cd6d15be8fd88
Rhys <rhpowell@redhat.com>
2024-01-04 16:30:59 +0000

    fixed output, removed the whole cluster output and pulled out data as nothing was shown due to sensitive data

--- rosa_sts_unmanaged_oidc/output.tf
+++ rosa_sts_unmanaged_oidc/output.tf
@@ -1,18 +1,8 @@
-output "rosa_cluster" {
-  value = { cluster = [
-    {
-      rosa_sts_cluster = module.rosa_cluster
-    },
-    {
-      cluster_id = module.rosa_cluster.cluster_id
-    } /*,
-    {
-      operator_iam_roles    = module.operator_roles_and_oidc.operator_iam_roles
-    }*/
-  ] }
+##
+output "cluster_id" {
+  value = module.rosa_cluster.cluster_id
 }
 
-##
 output "oidc_config_id" {
   value = module.oidc_provider.id
 }
@@ -25,15 +15,19 @@ output "thumbprint" {
   value = module.oidc_provider.thumbprint
 }
 
-output admin_username {
+output "admin_username" {
   value = module.rosa_cluster.admin_username
 }
 
-output admin_password {
-  value = module.rosa_cluster.admin_password
+output "admin_password" {
+  value     = module.rosa_cluster.admin_password
   sensitive = true
 }
 
-output api_url {
+output "api_url" {
   value = module.rosa_cluster.api_url
+}
+
+output "domain" {
+  value = module.rosa_cluster.domain
 }
